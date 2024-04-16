0e49ce644105ea354817ff6722534ab4ae106004
matheus <matheuscosta.tech@gmail.com>
2024-04-15 21:26:05 -0300

    Removing logs

--- lib/screens/members.dart
+++ lib/screens/members.dart
@@ -149,7 +149,6 @@ class _MembersScreenState extends ConsumerState<MembersScreen> {
 
   @override
   Widget build(BuildContext context) {
-    print('members screen build');
     return Scaffold(
       appBar: AppBar(
         title: const Text(
