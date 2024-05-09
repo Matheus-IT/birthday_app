0e504baae036c3809c901bcfe213eb3ac20f61aa
matheus <matheuscosta.tech@gmail.com>
2024-05-09 00:38:31 -0300

    Sorting members by next birthday

--- lib/providers/members_provider.dart
+++ lib/providers/members_provider.dart
@@ -5,27 +5,52 @@ class MembersStateNotifier extends StateNotifier<List<Member>> {
   MembersStateNotifier() : super([]);
 
   void setMembers(List<Member> members) {
-    state = members;
+    state = _sortByNextBirthday(members);
   }
 
   void updateMember(Member updatedMember) {
     final myMembers = state;
     final index = myMembers.indexWhere((m) => m.id == updatedMember.id);
     myMembers[index] = updatedMember;
-    state = [...myMembers];
+    state = [..._sortByNextBirthday(myMembers)];
   }
 
   void addMember(Member newMember) {
     final myMembers = state;
     myMembers.add(newMember);
-    state = [...myMembers];
+    state = [..._sortByNextBirthday(myMembers)];
   }
 
   void removeMember(String memberId) {
     final myMembers = state;
     final index = myMembers.indexWhere((m) => m.id == memberId);
     myMembers.removeAt(index);
-    state = [...myMembers];
+    state = [..._sortByNextBirthday(myMembers)];
+  }
+
+  List<Member> _sortByNextBirthday(List<Member> members) {
+    members.sort((a, b) {
+      final now = DateTime.now();
+      final today = DateTime(now.year, now.month, now.day);
+
+      // Calculate difference from today for each member, ignoring year
+      final memberADifference = _calculateDifference(today, a.birthDate);
+      final memberBDifference = _calculateDifference(today, b.birthDate);
+
+      return memberADifference.compareTo(memberBDifference);
+    });
+    return members;
+  }
+
+  int _calculateDifference(DateTime today, DateTime memberBirthdate) {
+    // Helper function to calculate the difference ignoring year
+    final nextBirthday = DateTime(today.year, memberBirthdate.month, memberBirthdate.day);
+    if (nextBirthday.isBefore(today)) {
+      // Handle birthdays that already passed this year
+      return nextBirthday.difference(today).inDays + 365;
+    } else {
+      return nextBirthday.difference(today).inDays;
+    }
   }
 }
 
