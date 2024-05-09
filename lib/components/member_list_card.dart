ed09976570c864bd0d7e8c37a4f01c213dc92779
matheus <matheuscosta.tech@gmail.com>
2024-05-09 00:59:24 -0300

    Highlighting birthday members

--- lib/components/member_list_card.dart
+++ lib/components/member_list_card.dart
@@ -14,53 +14,59 @@ class MemberListCard extends ConsumerWidget {
   final Future<void> Function(Member member) onDeleteMember;
   final void Function(Member member) onEditMember;
 
+  void handleShowExtraInfoDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (ctx) {
+        return SimpleDialog(
+          contentPadding: const EdgeInsets.only(top: 32, right: 32, left: 32, bottom: 16),
+          title: const Text('Mais informações'),
+          children: [
+            Text('Nome: ${member.name}'),
+            Text('Data de nascimento: ${member.birthDateReadableFull}'),
+            Text('Telefone: ${member.phoneNumber}'),
+            const SizedBox(height: 16),
+            TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.red,
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Deletar membro'),
+              onPressed: () {
+                onDeleteMember(member);
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Fechar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  bool _isMemberBirthday(Member member) {
+    final now = DateTime.now();
+    return member.birthDate.day == now.day && member.birthDate.month == now.month;
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    void handleShowExtraInfoDialog() {
-      showDialog(
-        context: context,
-        builder: (ctx) {
-          return SimpleDialog(
-            contentPadding: const EdgeInsets.only(top: 32, right: 32, left: 32, bottom: 16),
-            title: const Text('Mais informações'),
-            children: [
-              Text('Nome: ${member.name}'),
-              Text('Data de nascimento: ${member.birthDateReadableFull}'),
-              Text('Telefone: ${member.phoneNumber}'),
-              const SizedBox(height: 16),
-              TextButton(
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.red,
-                  textStyle: Theme.of(context).textTheme.labelLarge,
-                ),
-                child: const Text('Deletar membro'),
-                onPressed: () {
-                  onDeleteMember(member);
-                  Navigator.of(context).pop();
-                },
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  textStyle: Theme.of(context).textTheme.labelLarge,
-                ),
-                child: const Text('Fechar'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
-      );
-    }
-
     return Card(
+      color: _isMemberBirthday(member) ? Colors.purple[100] : null,
       child: ListTile(
-        onTap: handleShowExtraInfoDialog,
+        onTap: () => handleShowExtraInfoDialog(context),
         leading: CircleAvatar(
           child: member.profilePicturePath.isNotEmpty
               ? Image.network(member.profilePicturePath)
-              : const Icon(Icons.person),
+              : Icon(_isMemberBirthday(member) ? Icons.cake : Icons.person),
         ),
         title: Text(member.name),
         subtitle: Text('Data de aniversário ${member.birthDateReadable}'),
