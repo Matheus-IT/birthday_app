a5c5d5159c46d36c164e3ea9abaf270774cc3550
matheus <matheuscosta.tech@gmail.com>
2024-05-08 23:59:06 -0300

    New url for birthday members

--- lib/api_urls.dart
+++ lib/api_urls.dart
@@ -4,6 +4,7 @@ class ApiUrls {
   static String base = dotenv.env['BASE_URL']!;
   static Uri login = Uri.parse('${base}login/');
   static Uri members = Uri.parse('${base}members/');
+  static Uri birthdayMembers = Uri.parse('${base}get-birthdays-of-the-day/');
 
   static Uri member(String id) {
     return Uri.parse('${base}members/$id/');
